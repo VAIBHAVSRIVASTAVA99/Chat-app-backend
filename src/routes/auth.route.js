3cf2762279804f30496334362e3452e33b012c0a
VAIBHAVSRIVASTAVA99 <srivastavavaibhav499@gmail.com>
2025-02-16 23:22:34 +0530

    hellu

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -2,8 +2,6 @@ import express from "express";
 import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 import cors from "cors";
-app.use(cors());
-
 const router = express.Router();
 
 router.post("/signup", signup);
